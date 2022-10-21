33d72773a3f6313a30c3e373d6922cf331516e67
dominion321 <dominion.ikpeawujor@stu.cu.edu.ng>
2022-10-21 16:08:43 +0100

    added new routes for the backend

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ const app = express();
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser:true});
 
+//Schema Declaration
+const BlogPost = require('./models/BlogPost.js');
+
+//Port number declaration
+const port = 3000;
+
+//Body Parser Library
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 //Templating Engine
 app.set('view engine', 'ejs');
 
 //Static files (css, img, code)
 app.use(express.static('public'));
 
-
 //App Endpoints
-app.get('/', (req,res) => {
-    res.render('index');
+app.get('/', async (req,res) => {
+    const blogposts = await BlogPost.find({});
+    console.log(blogposts, {blogposts});
+    res.render('index', {blogposts});
 });
 
 app.get('/about', (req,res) => {
@@ -28,6 +37,10 @@ app.get('/contact', (req,res) => {
     res.render('contact');
 });
 
+//test
+app.get('/test',(req,res) => {
+    res.render()
+})
 app.get('/post', (req,res) => {
     res.render('post');
 })
@@ -36,12 +49,33 @@ app.get('/post/new', (req,res) => {
     res.render('create');
 });
 
-app.post('/posts/store',(req,res) => {
-    console.log(req.body);
-    res.render('/');
+// app.get('/post/search',async (req,res) => {
+//     const blogposts = await BlogPost.find({})
+//     res.render('test', {blogposts});
+// })
+
+// app.get('/post/search/:id',async (req,res) => {
+//     const blogposts = await BlogPost.find(/req.body.title/)
+//     res.render('test', {blogposts});
+// })
+
+app.post('/posts/store',async (req,res) => {
+    await BlogPost.create(req.body, (error,blogpost) =>{
+        res.redirect('/');
+    })
 });
 
+app.get('/post', (req,res) =>{
+    res.render('/');
+})
+
+app.get('/post/:id', async (req,res) => {
+    const blogpost = await BlogPost.findById(req.params.id);
+    console.log(blogpost, "Oshey ooo");
+    res.render('post' ,{blogpost});
+})
+
 //Server Spin-up
-app.listen(3000,() => {
-    console.log("App listening at Port 3000");
+app.listen(port,() => {
+    console.log(`App listening at Port ${port}`);
 })
--- test.js
+++ test.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const BlogPost = require('./models/BlogPost');
-mongoose.connecy('mongodb://localhost/my_database/',{UseNewUrlParser:true});
+mongoose.connect('mongodb://localhost/my_database',{UseNewUrlParser:true});
 
 BlogPost.create({
-    title:"The Story of a Young Inquisite Softwrae Billionaire.",
+    title:"The Story of a Young Inquisite Software Billionaire.",
     body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione provident vel debitis odio, nihil vitae! Velit, voluptatibus nesciunt veritatis similique voluptatem mollitia placeat quod nihil, illo architecto magnam fuga optio?"
 },(error, blogpost) => {
-    console.log(err,blogpost);
+    console.log(error,blogpost);
 })
+
+// var id = '6350315017c72b36ff8c0f19';
+// BlogPost.findByIdAndDelete(id, (error, blogpost) => {
+//     console.log(error, blogpost);
+// });
